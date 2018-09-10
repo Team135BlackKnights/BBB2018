8c5da0faaa097c13a58aadfeb146401b7f5983d4
KEATON <keatfish@gmail.com>
2018-09-10 14:44:31 -0400

    Hih

---
+++ BBB2018/src/org/usfirst/frc/team135/robot/subsystems/DRIVETRAIN.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team135.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import org.usfirst.frc.team135.robot.RobotMap;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.PIDOutput;
+
+public class DRIVETRAIN extends Subsystem {
+
+	static DriveTrain instance;
+	
+	WPI_TalonSRX frontLeftMotor = new 
+}
