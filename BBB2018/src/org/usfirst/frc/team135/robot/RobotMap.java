7fc0af63e135e08f12cde085526429e26c68338b
dsimonetti04 <dsimonetti04@PHS142-022951.phmschools.org>
2018-09-10 14:18:48 -0400

    saa

--- BBB2018/src/org/usfirst/frc/team135/robot/RobotMap.java
+++ BBB2018/src/org/usfirst/frc/team135/robot/RobotMap.java
@@ -13,14 +13,12 @@ package org.usfirst.frc.team135.robot;
  * the wiring easier and significantly reduces the number of magic numbers
  * floating around.
  */
-public class RobotMap {
-	// For example to map the left and right motors, you could define the
-	// following variables to use with your drivetrain subsystem.
-	// public static int leftMotor = 1;
-	// public static int rightMotor = 2;
-
-	// If you are using multiple modules, make sure to define both the port
-	// number and the module. For example you with a rangefinder:
-	// public static int rangefinderPort = 1;
-	// public static int rangefinderModule = 1;
+public interface RobotMap 
+{
+	public interface K_OI
+	{
+		public static final double DEADBAND = .05;
+		public static final	int LEFT = 0, RIGHT = 1, MANIP = 2;
+		public static final int BACK_LEFT = 0, BACK_RIGHT = 1, FRONT_LEFT = 2, FRONT_RIGHT = 3;
+	}
 }
