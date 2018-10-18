3825ad182551a4dbf4a463c3ce0103e06b1c4ff5
David <dsimonetti04@phm.k12.in.us>
2018-10-18 11:01:23 -0400

    d

--- BBB2018/src/org/usfirst/frc/team135/robot/commands/auto/singles/DriveForward.java
+++ BBB2018/src/org/usfirst/frc/team135/robot/commands/auto/singles/DriveForward.java
@@ -24,17 +24,20 @@ public class DriveForward extends InstantCommand implements RobotMap
 		timer.start();
 		Robot.drivetrain.TankDrive(1.0, 1.0);
 		double time = timer.get();
-		while (distancetravelled < distance / 12 && timer.get() - time > .2 && finaltimer.get() < 2)
+		while ( finaltimer.get() < 2)
 		{
-			double currentvoltage = DriveTrain.frontRightMotor.getMotorOutputVoltage();
-			double estimatedvelocity = (currentvoltage - 1.25) * 1.25;
-			distancetravelled += estimatedvelocity * .2;
-			double error = (distance - distancetravelled) / distance;
-			Robot.drivetrain.TankDrive(1.0 * error, 1.0 * error);
-			System.out.println("Voltage: " + currentvoltage +
-					" Estimated Velocity: " + estimatedvelocity + 
-					" Distance Travelled: " + distancetravelled +"\n");
-			time = timer.get();
+			while (distancetravelled < distance / 12 && timer.get() - time > .2)
+			{
+				double currentvoltage = DriveTrain.frontRightMotor.getMotorOutputVoltage();
+				double estimatedvelocity = (currentvoltage - 1.25) * 1.25;
+				distancetravelled += estimatedvelocity * .2;
+				double error = (distance - distancetravelled) / distance;
+				Robot.drivetrain.TankDrive(1.0 * error, 1.0 * error);
+				System.out.println("Voltage: " + currentvoltage +
+						" Estimated Velocity: " + estimatedvelocity + 
+						" Distance Travelled: " + distancetravelled +"\n");
+				time = timer.get();
+			}
 		}
 	}
 	protected void end()
